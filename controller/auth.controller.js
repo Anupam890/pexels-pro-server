158816f360b6fcef49ad5819f1b3808fae28c30b
Anupam890 <anupamm059@gmail.com>
2026-01-13 00:42:52 +0530

    Fix: Configure Google OAuth and ignore secrets

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -7,38 +7,38 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
-        const email = profile.emails[0].value;
-        let user = await User.findOne({ email });
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/auth/google/callback"
+// }, async (accessToken, refreshToken, profile, done) => {
+//     try {
+//         const email = profile.emails[0].value;
+//         let user = await User.findOne({ email });
 
-        if (user) {
-            if (!user.googleId) {
-                user.googleId = profile.id;
-                user.provider = 'google';
-                await user.save();
-            }
-            return done(null, user);
-        }
+//         if (user) {
+//             if (!user.googleId) {
+//                 user.googleId = profile.id;
+//                 user.provider = 'google';
+//                 await user.save();
+//             }
+//             return done(null, user);
+//         }
 
-        user = await User.create({
-            name: profile.displayName,
-            email: email,
-            googleId: profile.id,
-            provider: 'google',
-            isVerified: true,
-            avatar: profile.photos[0]?.value || ""
-        });
+//         user = await User.create({
+//             name: profile.displayName,
+//             email: email,
+//             googleId: profile.id,
+//             provider: 'google',
+//             isVerified: true,
+//             avatar: profile.photos[0]?.value || ""
+//         });
 
-        return done(null, user);
-    } catch (error) {
-        return done(error, null);
-    }
-}));
+//         return done(null, user);
+//     } catch (error) {
+//         return done(error, null);
+//     }
+// }));
 
 
 export const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
